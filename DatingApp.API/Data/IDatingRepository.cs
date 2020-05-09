e2b9d318d17022286a7671dcc3febb4cec5cf843
ShamilEP <ShamilEP@users.noreply.github.com>
2020-05-09 01:37:47 -0400

    End of Section 11

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -9,7 +9,9 @@ namespace DatingApp.API.Data
          void Add<T>(T entity) where T : class;
          void Delete<T>(T entity) where T : class;
          Task<User> GetUser(int id);
+         Task<Photo> GetPhoto(int id);
          Task<IEnumerable<User>> GetUsers();
          Task<bool> SaveAll();
+         Task<Photo> GetMainPhotoForUser(int userId);
     }
 }
